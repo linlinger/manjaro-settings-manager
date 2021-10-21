54eae17f0b7301de88479b162b94b691d20f02c9
林凌儿 <32541118+linlinger@users.noreply.github.com>
2021-10-21 14:44:07 +0800

    Update LocalePage.cpp

--- src/modules/locale/LocalePage.cpp
+++ src/modules/locale/LocalePage.cpp
@@ -388,7 +388,7 @@ LocalePage::save()
         //Hide close button
         progDlg.setWindowFlags(progDlg.windowFlags() & ~Qt::WindowCloseButtonHint);
         disconnect(this, SIGNAL(canceled()), this, SLOT(cancel()));
-        progDlg.setWindowTitle(tr("Please wait while applying changes..."));
+        progDlg.setWindowTitle( QString( tr("Please wait while operation is in progress"));
         progDlg.setFixedWidth(300);
         progDlg.setRange(0, 0);
         progDlg.show();
